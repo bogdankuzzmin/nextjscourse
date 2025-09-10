2d0b761c32236c66a63575439184fc8fa6acf8db
Bogdan <bogdankuzzmin@gmail.com>
2025-09-10 19:55:12 +0500

    header background as separate component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,28 +1,34 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import HeaderBackground from '@/components/header/headerBackground/headerBackground';
+
 import logoImage from '@/assets/logo.png';
 import classes from './header.module.css';
 
 const Header = () => {
   return (
-    <header className={classes.header}>
-      <Link href="/" className={classes.logo}>
-        <Image src={logoImage} alt="logo" priority />
-        NextLevel Food
-      </Link>
+    <>
+      <HeaderBackground />
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImage} alt="logo" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
