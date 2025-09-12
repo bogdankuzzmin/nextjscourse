de164b094398d94b83e72e35ececc13e27479f68
Bogdan <bogdankuzzmin@gmail.com>
2025-09-12 19:35:37 +0500

    work on meals page, meal item

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -1,8 +1,29 @@
+import classes from './page.module.css';
+import Link from 'next/link';
+import Meals from '@/components/meals';
+
 const MealsPage = () => {
   return (
-    <main>
-      <h1>Meals Page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created {' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+
+        <p className={classes.cta}>
+          <Link href={'/meals/share'}>
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+
+      <main className={classes.main}>
+        <Meals meals={[]} />
+      </main>
+    </>
   );
 };
 
---
+++ src/components/meals/index.tsx
@@ -0,0 +1,16 @@
+import classes from './meals.module.css';
+import MealItem from '@/components/meals/mealItem';
+
+const Meals = ({ meals }) => {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
+      ))}
+    </ul>
+  )
+};
+
+export default Meals;
