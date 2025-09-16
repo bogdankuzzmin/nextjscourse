a1cdabbef9304e0982ff4c5d5ea66050de464110
Bogdan <bogdankuzzmin@gmail.com>
2025-09-16 22:47:05 +0500

    meal page

--- src/app/meals/[mealSlug]/page.tsx
+++ src/app/meals/[mealSlug]/page.tsx
@@ -1,9 +1,37 @@
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '../../../../lib/meals';
+
 const MealPage = ({ params }) => {
+  const meal = getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
   return (
-    <main>
-      <h1>Meal Page</h1>
-      <p>{params.mealSlug}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+
+          <p className={classes.creator}>
+            <a href={`mailto:${meal.creator_email}`}>
+              {meal.creator}
+            </a>
+          </p>
+
+          <p className={classes.summary}>
+            {meal.summary}
+          </p>
+        </div>
+      </header>
+
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{ __html: meal.instructions }} />
+      </main>
+    </>
   );
 };
 
